fd4e146ac97dba0ac8024c940b0e1ca5f6bf2a57
Dannel Albert <cuebix@gmail.com>
2020-05-24 11:40:02 -0400

    ^ navigate

--- src/ComputersPage.js
+++ src/ComputersPage.js
@@ -1,15 +1,11 @@
 import React, { useContext } from 'react'
 import { RainForestRouterContext } from './RainForestRouterContext'
 
-export const ComputersPage = () => {
-  const router = useContext(RainForestRouterContext)
-
-  return (
-    <>
-      <h2>Computers</h2>
-      <div>
-        <button onClick={() => router.navigate('/product/PBP-2020-01')}>PearBook Amateur</button>
-      </div>
-    </>
-  )
-}
+export const ComputersPage = ({ navigate = useContext(RainForestRouterContext).navigate }) => (
+  <>
+    <h2>Computers</h2>
+    <div>
+      <button onClick={() => navigate('/product/PBP-2020-01')}>PearBook Amateur</button>
+    </div>
+  </>
+)
