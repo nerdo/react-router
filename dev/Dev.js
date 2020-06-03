9d6cdf1c68f373770c2212d555b60dc32cee2630
Dannel Albert <cuebix@gmail.com>
2020-06-03 16:33:21 -0400

    * Testing async (deferred) router loading

--- dev/Dev.js
+++ dev/Dev.js
@@ -1,4 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { RainForest } from '../src/RainForest'
+import { makeReactUrlRouter } from '../src/makeReactUrlRouter'
 
-export const Dev = () => <RainForest />
+export const Dev = () => {
+  const [router, setRouter] = useState(null)
+
+  // Simulating loading router settings from some async call...
+  useEffect(
+    () => {
+      setTimeout(() => setRouter(makeReactUrlRouter()), 500)
+    },
+    []
+  )
+
+  return <RainForest router={router} />
+}
