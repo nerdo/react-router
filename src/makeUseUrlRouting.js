d4bb2715f7f38fc0b19f5216c55a339452215b2b
Dannel Albert <cuebix@gmail.com>
2020-06-03 22:55:13 -0400

    * simplified makeUseUrlRouting

--- src/makeUseUrlRouting.js
+++ src/makeUseUrlRouting.js
@@ -2,18 +2,12 @@ import { useState, useEffect } from 'react'
 import { getJoinedPath } from '@nerdo/js-routing'
 
 export const makeUseUrlRouting = ({ jsRouter }) => (routes) => {
-  const [navigationTarget, setNavigationTarget] = useState(void 0)
+  const [, setNavigationTarget] = useState(void 0)
 
   jsRouter.history.events.once('navigation', setNavigationTarget)
 
   // Apply routing, but do not auto-commit. Commit will happen after each render (useEffect)
   // ensuring that nested routes will have the proper base id for navigation.
   useEffect(jsRouter.commitRouting)
-  const result = jsRouter.applyRouting(routes, false)
-
-  // The current base id must be preserved in state.
-  const [currentBaseId] = useState(jsRouter.getCurrentBaseId())
-  const navigate = async input => jsRouter.navigate(getJoinedPath(currentBaseId, input))
-
-  return [result, navigate]
+  return jsRouter.applyRouting(routes, false)
 }
