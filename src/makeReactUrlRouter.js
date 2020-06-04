2b569c57822c57cbbdcea722708383d237731c81
Dannel Albert <cuebix@gmail.com>
2020-06-03 22:53:41 -0400

    + makeNavigationFunction, makeNavigationLink

--- src/makeReactUrlRouter.js
+++ src/makeReactUrlRouter.js
@@ -1,12 +1,17 @@
 import { makeUrlRouter } from '@nerdo/js-routing'
 import { makeUseUrlRouting } from './makeUseUrlRouting'
+import { makeNavigationLink } from './makeNavigationLink'
 
 export const makeReactUrlRouter = args => {
     const jsRouter = makeUrlRouter(args)
     const useUrlRouting = makeUseUrlRouting({ jsRouter })
 
-    return {
+    const reactUrlRouter = {
         useUrlRouting,
+        makeNavigationFunction: () => jsRouter.makeRouterNavigationFunction(jsRouter),
+        makeNavigationLink: ({ ...args } = {}) => makeNavigationLink({ router: reactUrlRouter, ...args }),
         ...jsRouter
     }
+
+    return reactUrlRouter
 }
